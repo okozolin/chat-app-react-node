6ede98f21acb578e171d9c364501d99305c04d82
Orit Kozolin <orit@nachum.co.il>
2020-07-15 09:20:17 +0300

    helper functions

---
+++ server-chat-app/helpers/users.js
@@ -0,0 +1,29 @@
+const users = [];
+
+const addUser = ({ id, nickname, room }) => {
+  nickname = nickname ? nickname.trim().toLowerCase() : null;
+
+  const existingUser = users.find(
+    (user) => user.room === room && user.nickname === nickname
+  );
+
+  if (existingUser) {
+    return { error: "Nickname already taken" };
+  }
+
+  const user = { id, nickname };
+  users.push(user);
+  console.log("{user}", { user });
+  return { user };
+};
+
+const removeUser = (id) => {
+  const index = users.findIndex((user) => user.id === id);
+  if (index != -1) {
+    return users.splice(index, 1)[0];
+  }
+};
+
+const getUser = (id) => users.find((user) => user.id === id);
+
+module.exports = { addUser, removeUser, getUser };
